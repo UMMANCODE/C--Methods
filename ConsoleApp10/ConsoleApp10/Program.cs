ff8f48b96e384f7f428cbb209e7d4cbe188303aa
ummanmemmedov2005@gmail.com <ummanmm@code.edu.az>
2023-12-27 22:52:46 +0400

    Files added

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace ConsoleApp10 {
+    internal class Program {
+        static void Main(string[] args) {
+            Calculate(5, 6, '+');
+            Console.WriteLine(Count("Hello World", 'l'));
+            Console.WriteLine(CountWords("Hello World"));
+            Console.WriteLine(SumOfDigits(123));
+            Console.WriteLine(Power(2, 3));
+
+            int[] array = ConvertToPositive(new int[] { 1, 2, 3, -4, -5, 6, -7, 8, 9 });
+            for (int i = 0; i < array.Length; i++) {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(Reverse("Hello World"));
+
+        }
+
+        #region Task1
+
+        // Verilmiş iki ədəd üzərində verilmiş operator simvoluna əsasən riyazi əməliyyat aparıb nəticəsini console-da göstərən metod.
+
+        static void Calculate(int a, int b, char op) {
+            switch (op) {
+                case '+':
+                    Console.WriteLine(a + b);
+                    break;
+                case '-':
+                    Console.WriteLine(a - b);
+                    break;
+                case '*':
+                    Console.WriteLine(a * b);
+                    break;
+                case '/':
+                    Console.WriteLine(a / b);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Task2
+
+        // Verilmiş yazıdaki verilmiş simvolun sayını qaytaran metod
+
+        static int Count(string str, char ch) {
+            int count = 0;
+            for (int i = 0; i < str.Length; i++) {
+                if (str[i] == ch) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        #endregion
+
+        #region Task3
+
+        // Verilmiş yazıdaki sözlərin sayını qaytaran metod
+
+        static int CountWords(string str) {
+            int count = 0;
+            for (int i = 0; i < str.Length; i++) {
+                if (str[i] == ' ') {
+                    count++;
+                }
+            }
+            return count + 1;
+        }
+
+        #endregion
+
+        #region Task4
+
+        // Verilmiş  ədədin rəqəmləri cəmini qaytaran metod
+
+        static int SumOfDigits(int num) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        }
+
+        #endregion
+
+        #region Task5
+
+        // Verilmiş ədədi verilmiş qüvvətə yüksəldib nəticəsini qaytaran metod
+
+        static int Power(int num, int pow) {
+            int result = 1;
+            for (int i = 0; i < pow; i++) {
+                result *= num;
+            }
+            return result;
+        }
+
+        #endregion
+
+        #region Task6
+
+        // Verilmiş ədədlər siyahısının bütün elementlərini müsbət şəkildə qaytaran metod.
+
+        static int[] ConvertToPositive(int[] arr) {
+            int[] result = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++) {
+                if (arr[i] < 0) result[i] = -1 * arr[i];
+                else result[i] = arr[i];
+            }
+            return result;
+        }
+
+        #endregion
+
+        #region Task7
+
+        // Verilmiş yazını tərs formada qaytaran metod
+
+        static string Reverse(string str) {
+            string result = "";
+            for (int i = str.Length - 1; i >= 0; i--) {
+                result += str[i];
+            }
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+
+namespace ConsoleApp9 {
+    internal class Program {
+        static void Main(string[] args) {
+            // Calling all the methods in Main method
+            Console.WriteLine(Sum(5, 6, 7));
+            Console.WriteLine(Sum(5.5, 6.6, 7.7));
+            Console.WriteLine(Sum(new int[] { 1, 2, 3, 4, 5 }));
+            Console.WriteLine(Sum(new double[] { 1.1, 2.2, 3.3, 4.4, 5.5 }));
+            Console.WriteLine(IndexOf("Hello World", 'o'));
+            Console.WriteLine(LastIndexOf("Hello World", 'o'));
+            Console.WriteLine(Max(new int[] { 1, 2, 3, 4, 5 }));
+            Console.WriteLine(IsPositiveAndEven(6));
+            Console.WriteLine(CountPositiveAndEven(new int[] { 1, 2, 3, 4, 5 }));
+            Console.WriteLine(string.Join(", ", GetPositiveAndEven(new int[] { 1, 2, 3, 4, 5 })));
+            Console.WriteLine(string.Join(", ", Reverse(new int[] { 1, 2, 3, 4, 5 })));
+            Console.WriteLine(TrimStart("    Hello World"));
+            Console.WriteLine(TrimEnd("Hello World    "));
+        }
+
+        #region Task1
+
+        // Verilmiş 3 ədədin cəmini tapan method(həm int, həm double ədədlər üçün)
+
+        static int Sum(int a, int b, int c) {
+            return a + b + c;
+        }
+
+        static double Sum(double a, double b, double c) {
+            return a + b + c;
+        }
+
+        #endregion
+
+        #region Task2
+
+        // Verilmiş ədədlər siyahısındaki bütün ədədlərin cəmini tapan method (həm int, həm double ədədlər üçün)
+
+        static int Sum(int[] arr) {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++) {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+        static double Sum(double[] arr) {
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++) {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+        #endregion
+
+        #region Task3
+
+        // Verilmiş yazıda veirlmiş charın yerləşdiyi ilk indexi tapan method(yoxdursa -1 qaytarır)
+
+        static int IndexOf(string str, char ch) {
+            for (int i = 0; i < str.Length; i++) {
+                if (str[i] == ch) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        #endregion
+
+        #region Task4
+
+        // Verilmiş yazıda veirlmiş charın yerləşdiyi son indexi tapan method (yoxdursa -1 qaytarır)
+
+        static int LastIndexOf(string str, char ch) {
+            for (int i = str.Length - 1; i >= 0; i--) {
+                if (str[i] == ch) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        #endregion
+
+        #region Task5
+
+        // Verilmiş ədədlər siyahısındaki ən böyük ədədi tapan method
+
+        static int Max(int[] arr) {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+
+        #endregion
+
+        #region Task6
+
+        // Verilmiş ədədin müsbət və cüt olub olmadığını tapan method
+
+        static bool IsPositiveAndEven(int num) {
+            return num > 0 && num % 2 == 0;
+        }
+
+        #endregion
+
+        #region Task7
+
+        // Verilmiş ədədlər siyahısındaki müsbət və cüt ədədlərin sayını tapan method
+
+        static int CountPositiveAndEven(int[] arr) {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++) {
+                if (IsPositiveAndEven(arr[i])) {  // Evvelki metoddan istifadə edirik
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        #endregion
+
+        #region Task8
+
+        // Verilmiş ədədlər siyahısındaki müsbət və cüt ədədlərdən ibarət yeni bir array düzəldən method.
+
+        static int[] GetPositiveAndEven(int[] arr) {
+            int[] newArr = new int[CountPositiveAndEven(arr)];  // Evvelki metoddan istifadə edirik
+            int index = 0;
+            for (int i = 0; i < arr.Length; i++) {
+                if (IsPositiveAndEven(arr[i])) {  // Evvelki metoddan istifadə edirik
+                    newArr[index++] = arr[i];
+                }
+            }
+            return newArr;
+        }
+
+        #endregion
+
+        #region Task9
+
+        // Verilmiş ədədlər siyahısından tərsinə düzülmüs yeni bir array qaytaran method.
+
+        static int[] Reverse(int[] arr) {
+            int[] newArr = new int[arr.Length];
+            int index = 0;
+            for (int i = arr.Length - 1; i >= 0; i--) {
+                newArr[index++] = arr[i];
+            }
+            return newArr;
+        }
+
+        #endregion
+
+        #region Task10
+
+        // Verilmiş yazının əvvəlindəki boşluqlar silinmiş bir string düzəldən method
+
+        static string TrimStart(string str) {
+            string newStr = "";
+            int index = 0;
+            while (str[index] == ' ') {
+                index++;
+            }
+            for (int i = index; i < str.Length; i++) {
+                newStr += str[i];
+            }
+            return newStr;
+        }
+
+        #endregion
+
+        #region Task11
+
+        // Verilmiş yazıdan sonundaki boşluqlar silinmiş yeni string düzəldən method
+
+        static string TrimEnd(string str) {
+            string newStr = "";
+            int index = str.Length - 1;
+            while (str[index] == ' ') {
+                index--;
+            }
+            for (int i = 0; i <= index; i++) {
+                newStr += str[i];
+            }
+            return newStr;
+        }
+
+        #endregion
+
+    }
+}
